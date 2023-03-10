e776f6cf4ef487942714a0bad8128c02aaf96135
Noclippy.T <72783888+NoclippyT@users.noreply.github.com>
2023-03-10 17:47:57 +0200

    Update Loader.lua

---
+++ Slasher Blade [12336036424]/Loader.lua
@@ -0,0 +1,8 @@
+--[[
+
+<<----------- Made by NoclippyT ----------->>
+Please do not link-shrink my scripts, thanks.
+
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoclippyT/Scripts/main/Slasher%20Blade%20%5B12336036424%5D/Main.lua"))()
