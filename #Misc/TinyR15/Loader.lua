298ae7d35fd2d18f87467540dbfda6634f30f53a
Noclippy.T <72783888+NoclippyT@users.noreply.github.com>
2023-03-13 19:36:56 +0200

    Update Loader.lua

--- #Misc/TinyR15/Loader.lua
+++ #Misc/TinyR15/Loader.lua
@@ -1,10 +1,15 @@
 --[[
+
+<<----------- Made by NoclippyT ----------->>
+Please do not link-shrink my scripts, thanks.
+
 # This script only works with the R15 body type.
 # The script execution can be detected by games serversided.
 # If the script isn't working you might need to set heigher values depanding on how long-
 does it take to your character to fully load once it spawns. (I recommend to use 1.1 multipliers)
 
 Avatar scale settings to become small as possible:
+(https://www.roblox.com/my/avatar)
 
 Body Type: 0%
 Height: 90%
